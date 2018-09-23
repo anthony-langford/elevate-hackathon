34b8b9401a6286e75a7619f1d302f9e3d981b9de
ralph-dev <ralph.maamari@mail.utoronto.ca>
2018-09-22 21:35:27 -0400

    Working Part!

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import { Router } from "@reach/router";
 // Import components
 import Home from './components/Home';
 import Report from './components/Report';
+// import D from './components/Report';
 
 import './styles/normalize.css';
 
-const App = () =>
+const App = () => (
   <Router id="router">
     <Home path="/" />
     <Report path="/report" />
+    {/* <Details path="/details" /> */}
   </Router>
+)
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { List } from 'semantic-ui-react'
 import axios from 'axios';
+import { Router, navigate } from "@reach/router";
 
-const ListExampleDivided = () => (
+const ListExampleDivided = (props) => (
   <List divided relaxed>
     <List.Item>
       <List.Icon name='github' size='large' verticalAlign='middle' />
-      <List.Content>
+      <List.Content onClick={() => props.clicked(5)}>
         <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
         <List.Description as='a'>Updated 10 mins ago</List.Description>
       </List.Content>
@@ -31,14 +32,41 @@ const ListExampleDivided = () => (
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      values: ""
+    }
   }
 
+  componentDidMount() {
+    // axios.get(`https://jsonplaceholder.typicode.com/users`)
+    //   .then(res => {
+    //     const persons = res.data;
+    //     this.setState({ persons });
+    //   })
+  }
 
+  onClick = (id) => {
+    navigate(`/issue/${id}`)
+  }
 
   render() {
     return (
       <React.Fragment>
-        <ListExampleDivided />
+        <div>
+          <div class='ui pointing menu'>
+            <a class='active item'>Recent Issues</a>
+            <a class='item'>Completed Issues</a>
+            <div class='right menu'>
+              <div class='item'>
+                <div class='ui icon input'>
+                  <input type='text' placeholder='Search...' />
+                  <i aria-hidden='true' class='search icon' />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <ListExampleDivided clicked={this.onClick} />
       </React.Fragment>
     );
   };
