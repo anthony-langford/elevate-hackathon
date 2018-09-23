375dd215d5c1c9567ada9a70669664500b04cef8
ralph-dev <ralph.maamari@mail.utoronto.ca>
2018-09-22 20:15:46 -0400

    new react ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,30 @@
 import React from 'react'
-import styled from 'styled-components'
+import { List } from 'semantic-ui-react'
 
+const ListExampleDivided = () => (
+  <List divided relaxed>
+    <List.Item>
+      <List.Icon name='github' size='large' verticalAlign='middle' />
+      <List.Content>
+        <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
+        <List.Description as='a'>Updated 10 mins ago</List.Description>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='github' size='large' verticalAlign='middle' />
+      <List.Content>
+        <List.Header as='a'>Semantic-Org/Semantic-UI-Docs</List.Header>
+        <List.Description as='a'>Updated 22 mins ago</List.Description>
+      </List.Content>
+    </List.Item>
+    <List.Item>
+      <List.Icon name='github' size='large' verticalAlign='middle' />
+      <List.Content>
+        <List.Header as='a'>Semantic-Org/Semantic-UI-Meteor</List.Header>
+        <List.Description as='a'>Updated 34 mins ago</List.Description>
+      </List.Content>
+    </List.Item>
+  </List>
+)
 
-export default class Home extends React.Component {
-  render() {
-
-    return (
-      <React.Fragment>
-        <div role='list' class='ui divided relaxed list'>
-  <div role='listitem' class='item'>
-    <i aria-hidden='true' class='github large icon middle aligned' />
-    <div class='content'>
-      <a class='header'>Semantic-Org/Semantic-UI</a>
-      <a class='description'>Updated 10 mins ago</a>
-    </div>
-  </div>
-  <div role='listitem' class='item'>
-    <i aria-hidden='true' class='github large icon middle aligned' />
-    <div class='content'>
-      <a class='header'>Semantic-Org/Semantic-UI-Docs</a>
-      <a class='description'>Updated 22 mins ago</a>
-    </div>
-  </div>
-  <div role='listitem' class='item'>
-    <i aria-hidden='true' class='github large icon middle aligned' />
-    <div class='content'>
-      <a class='header'>Semantic-Org/Semantic-UI-Meteor</a>
-      <a class='description'>Updated 34 mins ago</a>
-    </div>
-  </div>
-</div>
-      </React.Fragment>
-    )
-  }
-}
+export default ListExampleDivided;
